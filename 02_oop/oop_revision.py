e45e740640309c4746345dec8e0700f305f41735
Faraz Ahmad <farazahmad31048@gmail.com>
2024-08-11 23:07:05 +0500

    simple class

---
+++ 02_oop/oop_revision.py
@@ -0,0 +1,36 @@
+# OOP Revision Process;
+
+# class Car:
+#     def __init__(self, name, model, power, price):
+#         self.name = name
+#         self.model = model
+#         self.power = power
+#         self.price = price
+        
+    
+#     def printInfo(self):
+#         print(f"Name= {self.name} , Model= {self.model}, Power= {self.power}, Price= {self.power} .")
+
+
+# c = Car("Honda","Civic","1600",9000000)
+# c.printInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
