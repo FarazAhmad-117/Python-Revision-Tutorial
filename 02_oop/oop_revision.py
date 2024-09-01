df780f77dcb15691a957dfe11975945c53623877
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-09-01 10:32:35 +0500

    questions done

--- 02_oop/oop_revision.py
+++ 02_oop/oop_revision.py
@@ -1,19 +1,56 @@
 # OOP Revision Process;
 
-# class Car:
-#     def __init__(self, name, model, power, price):
-#         self.name = name
-#         self.model = model
-#         self.power = power
-#         self.price = price
+class Car:
+    def __init__(self, name, model, power, price):
+        self.name = name
+        self.model = model
+        self.power = power
+        self.price = price
         
     
-#     def printInfo(self):
-#         print(f"Name= {self.name} , Model= {self.model}, Power= {self.power}, Price= {self.power} .")
+    def printInfo(self):
+        print(f"Name= {self.name} , Model= {self.model}, Power= {self.power}, Price= {self.power} .")
 
 
-# c = Car("Honda","Civic","1600",9000000)
-# c.printInfo()
+c = Car("Honda","Civic","1600",9000000)
+c.printInfo()
+
+
+
+# ----------constructor--------------
+
+# ----------Inheritance----------------
+
+class ElectricCar(Car):
+    def __init__(self,name, model, power, price,battery_power, recharge_capacity):
+        super().__init__(name, model, power, price)
+        self.__battery_power = battery_power
+        self.recharge_capacity = recharge_capacity
+        
+    
+    def printInfo(self):
+        super().printInfo()
+        print(f"Battery Power = {self.__battery_power}, Recharge Capacity= {self.recharge_capacity}")
+
+e = ElectricCar("Tesla", "E900V", "1500", 12300000, "7500","20 hours")
+e.printInfo()
+
+
+
+
+# ----------------ENCAPSULATION-----------------------------
+
+#  JUst place __ before a variable and it becomes private
+
+# print(e.__battery_power)  # Generating Error
+
+
+
+
+
+
+
+
 
 
 
---
+++ 03_decorators/decorators.py
@@ -0,0 +1,53 @@
+# Decorators are going to be explained in this file;
+
+# How can you calculate the time of execution of a function;
+
+# import time
+
+
+# def timer(func):
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         result = func(*args, **kwargs)
+#         end_time = time.time()
+#         print(f"Time of execution for {func.__name__} >>> {end_time - start_time}")
+#         return result
+
+#     return wrapper
+
+
+# @timer
+# def example_function(n):
+#     time.sleep(n)
+
+
+# example_function(2)
+
+# Correct way!
+# fn = timer(example_function)
+# fn(2)
+
+
+# Question 2: Make a decorator debug to print fuction name and all of its arguments
+
+
+# def debug(func):
+#     def wrapper(*args, **kwargs):
+#         print(f"Funtion name is {func.__name__}")
+#         print(f"Args are >> {args}")
+#         print(f"KWArgs are >> {kwargs}")
+#         result = func(*args, **kwargs)
+#         return result
+
+#     return wrapper
+
+
+# @debug
+# def print_name_age(name, age):
+#     print(f"{name} is {age} years old !")
+
+
+# print_name_age("Faraz Ahmad", age=34)
+
+
+# Questions 3:
